df740359a55c5722e6e07515f85b0fb20fc6828c
Andrea Blasco <ablasco@fas.harvard.edu>
2018-03-09 01:17:03 -0500

    new function fule

---
+++ scripts/lib/functions.R
@@ -0,0 +1,38 @@
+
+require(xtable)
+
+# 
+percent <- function(x, ...) round(100*x, ...)
+
+# Render tables
+table_render <- function(x, caption, label, add=NULL, align=NULL, digits=1, notes=NULL, ...) {
+  if (is.null(align)) align <- c("@{}l",rep("c", ncol(x)))
+  table.head <- "\\\\[-1.8ex]\\hline \\hline \\\\[-1.8ex]\n"
+  table.mid <- "\\hline \\\\[-1.86ex]\n"
+  table.bottom <- table.head
+  add.to.cmd <- c(table.head, table.mid, table.bottom)
+  add.to.row <- list(pos = list(-1, 0, nrow(x))
+                , command = add.to.cmd)
+  if (!is.null(add)) {
+    add.to.row$command  <- c(add.to.row$command, add$cmd)
+    for (i in 1:length(add$pos)) {
+      add.to.row$pos[[3+i]] <- add$pos[[i]]
+    }
+  }
+  cat("\\begin{table}\n")   
+  cat("\\centering\n")                  
+  cat(sprintf("\\caption{%s}\n", caption))
+  cat(sprintf("\\label{%s}\n", label))
+  print(xtable(x, caption, label, align, digits)
+      , add.to.row=add.to.row
+      , hline.after=NULL
+      , floating=FALSE
+      , comment=FALSE
+      , ...)
+  if (!is.null(notes)) {
+    cat("\\begin{tablenotes}\n")                  
+    cat(sprintf("%s\n", notes))
+    cat("\\end{tablenotes}\n")
+  }
+  cat("\\end{table}\n")
+}  
